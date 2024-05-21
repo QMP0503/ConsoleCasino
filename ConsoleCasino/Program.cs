9c06f0490546ca3fc05633d2f09395379c6af348
QMP0503 <quangypham0503@gmail.com>
2024-05-21 17:54:25 +0700

    added deposit method and started dice game

--- ConsoleCasino/Program.cs
+++ ConsoleCasino/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+
+namespace ConsoleCasino
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            userAccount user;
+            void printOption()
+            {
+                Console.WriteLine("Please select the following options");
+                Console.WriteLine("1. Add deposit to balance");
+                Console.WriteLine("2. start playing DICEGAME");
+                Console.WriteLine("3. Exit Casino");
+            }
+            void addBalance()
+            {
+                try
+                {
+                    Console.WriteLine("Enter the amount you would like to deposit: ");
+                    double deposit = double.Parse(Console.ReadLine());
+                    user.setNewBalance(deposit);
+                    Console.WriteLine("Thank you for your deposit!");
+                }catch (Exception ex)
+                {
+                    Console.WriteLine("Please enter a numerical value.");
+                }
+                
+            }
+            void diceGame()
+            {
+                Console.WriteLine("Welcome to the DICE GAME!");
+                Console.WriteLine("Press the \"k\" key when you are ready to start");
+                Console.WriteLine("Press the \"n\" key when you are not ready");
+                string input = Console.ReadLine();
+                while(true)
+                {
+                    if (input.ToLower() == "k")
+                    {
+                        break;
+                    }
+                    else if (input.ToLower() == "n")
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("please enter a valid option");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleCasino/userAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCasino
+{
+    public class userAccount
+    {
+        string username;
+        int pin;
+        double balance;
+
+        public userAccount(string username, int pin, double balance)
+        {
+            this.username = username;
+            this.pin = pin;
+            this.balance = balance;
+        }
+        public int getPin() { return pin; }
+        public double getBalance() { return balance; }
+        public string getUsername() { return username; }
+        public void setUsername(string username) { this.username = username; }
+        public void setPin(int pin) {  this.pin = pin; }
+        public void setNewBalance(double deposit) {  balance += deposit; }
+    }
+}
